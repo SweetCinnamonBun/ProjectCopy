21157024d20f5982b2a2ad0b80190e2a45f2fce0
Roman <91057166+SweetCinnamonBun@users.noreply.github.com>
2024-03-19 10:34:55 +0200

    new commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/reset.css'
+import { ConfirmEmailPage } from './components/ConfirmEmailPage.jsx'
+import { ResetPasswordPage } from './components/ResetPasswordPage.jsx'
+import {Routes, Route } from "react-router-dom";
+
+import { AuthProvider } from './context/AuthContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ForgotPasswordPage } from './components/ForgotPasswordPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+)
